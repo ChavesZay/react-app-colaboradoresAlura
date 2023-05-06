1a56a19a101fc3bde79da8f31d5ccebbc361eb2e
Zaylin <chaveszay30@gmail.com>
2023-05-05 22:47:50 -0600

    Creando Footer

--- src/App.js
+++ src/App.js
@@ -1,87 +1,92 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css';
 import header from './componentes/header/header.js';
 import Form from './componentes/Form/Form.js'
 import MiOrg from './componentes/MiOrg';
 import Equipo from './componentes/Equipo';
+import Footer from './componentes/Footer';
 
 function App() {
   const equipos = [
     {
-      titulo:'Programación',
-      colorPrimario:"#57C278 ",
-      colorSecundario:"#D9F7E9" 
+      titulo: 'Programación',
+      colorPrimario: "#57C278 ",
+      colorSecundario: "#D9F7E9"
     },
     {
-      titulo:'Front End',
-      colorPrimario:"#82CFFA",
-      colorSecundario:"#E8F8FF" 
+      titulo: 'Front End',
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
     },
     {
-      titulo:'Data Science',
-      colorPrimario:"#A6D157",
-      colorSecundario:"#F0F8E2" 
+      titulo: 'Data Science',
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
     },
     {
-      titulo:'Devops',
-      colorPrimario:"#E06B69",
-      colorSecundario:"#FDE7E8" 
+      titulo: 'Devops',
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
     },
     {
-      titulo:'UX y Diseño',
-      colorPrimario:"#DB6EBF",
-      colorSecundario:"#FAE9F5" 
+      titulo: 'UX y Diseño',
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
     },
     {
-      titulo:'Móvil',
-      colorPrimario:"#FFBA05",
-      colorSecundario:"#FFF5D9" 
+      titulo: 'Móvil',
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
     },
     {
-      titulo:'Innovación y Gestión',
-      colorPrimario:"#FF8A29",
-      colorSecundario:"#FFEEDF" 
+      titulo: 'Innovación y Gestión',
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
     }
   ];
-  const [mostrarForm, actualizarMostar]=useState(false)
+  const [mostrarForm, actualizarMostar] = useState(false)
 
-  const [colaboradores,actualizarColaboradores]=useState([])
+  const [colaboradores, actualizarColaboradores] = useState([{
+    nombre: "Jeanmarie",
+    puesto: "Instructora",
+    foto: "https://github.com/JeanmarieAluraLatam.png",
+    equipo: "Front End"
+  }])
+
   //Ternaio --> condicion? seMuestra : noSeMuestra
   //condicion && seMuestra
-  const cambiarMostrar=()=>{
+  const cambiarMostrar = () => {
     actualizarMostar(!mostrarForm)
   }
 
   //Registrar Colaborador
-  const registrarColaborador=(colaborador)=>{
-    console.log("Nuevo colaborador",colaborador)
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo colaborador", colaborador)
     //Spread operator
     //... significa que esta copiando los valores actuales y añade el nuevo valor
     actualizarColaboradores([...colaboradores, colaborador])
   }
-    return (
+  return (
     <div >
-      {header() }
+      {header()}
       {/*mostrarForm === true ? <Form /> : <></>  una forma de hacerlo*/}
-      {mostrarForm && <Form 
-       equipos={equipos.map((equipo)=>equipo.titulo)}
-       registrarColaborador={registrarColaborador}/>  /*Otra forma de hacerlo*/}
-     <MiOrg cambiarMostrar={cambiarMostrar} />
+      {mostrarForm && <Form
+        equipos={equipos.map((equipo) => equipo.titulo)}
+        registrarColaborador={registrarColaborador} />  /*Otra forma de hacerlo*/}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
 
-     {
-      equipos.map((equipo)=>{
-        
-        return <Equipo  
-        datos={equipo} 
-        key={equipo.titulo}
-        colaboradores={colaboradores.filter(colaborador=> colaborador.equipo===equipo.titulo)} />
+      {
+        equipos.map((equipo) => {
 
-      })
-     }
+          return <Equipo
+            datos={equipo}
+            key={equipo.titulo}
+            colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)} />
 
-    
+        })
+      }
 
-
+      <Footer />
     </div>
   );
 }
